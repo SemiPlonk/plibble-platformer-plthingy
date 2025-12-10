cf71c31e3ecb2f4658fb558f4ec593ea885e54b9
JPlonk <giantdogy@hotmail.com>
2025-12-10 10:23:47 +0000

    Delete main/player directory

--- main/player/spawner/Spawnpoint.go
+++
@@ -1,8 +0,0 @@
-components {
-  id: "Player Spawner"
-  component: "/main/player/spawner/Player Spawner.factory"
-}
-components {
-  id: "Player spawning"
-  component: "/main/player/spawner/Player spawning.script"
-}
